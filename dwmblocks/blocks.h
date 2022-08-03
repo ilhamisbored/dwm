5b01d000fad433f7bde680ff6c6b5fb913d7679f
Ilham <68147233+ilhamisbored@users.noreply.github.com>
2022-08-03 14:35:50 +0600

    Add files via upload

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,11 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	1,		0},
+	{"  ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	1,		0},
 
-	{" ", "date '+%b %d %y %I:%M%p'",					1,		0},
+	{"  ", "date '+%b %d %y %I:%M%p'",					1,		0},
 
-	{" ", "/home/ilham/.config/dwmblocks/scripts/vol.sh", 1, 10},
+	{"  ", "/home/ilham/.config/dwmblocks/scripts/vol.sh", 1, 10},
+
+	{"  ", "/home/ilham/.config/dwmblocks/scripts/updates", 360, 9},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
